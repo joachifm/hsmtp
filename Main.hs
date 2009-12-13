9caf9fa1bf2a3b074a320f88257653ed7a4910b0
Joachim Fasting <joachim.fasting@gmail.com>
2009-12-13 10:54:38 +0100

    Main.hs: use withFirstDevice

--- Main.hs
+++ Main.hs
@@ -11,16 +11,13 @@ main :: IO ()
 main = do
     args <- getArgs
     case args of
-        ("list":_) -> do
-            mtp <- getFirstDevice
+        ("list":_) -> withFirstDevice $ \mtp -> do
             files <- getFileListing mtp
             mapM_ (\f -> printf "%d - %s - %d - %s\n" (fileID f) (fileName f)
                                  (fileSize f) (show (fileType f)))
                   files
-        ("get":fid:fname:_) -> do
-            mtp <- getFirstDevice
+        ("get":fid:fname:_) -> withFirstDevice $ \mtp -> do
             getFileToFile mtp (read fid) fname
-        ("send":fname:_) -> do
-            mtp <- getFirstDevice
+        ("send":fname:_) -> withFirstDevice $ \mtp -> do
             sendFileFromFile mtp fname
         _ -> putStrLn "Usage: Main <list|get|send> [ARGUMENT..]"
